c62877ea87084e3ff108da2ebbe879f1946af2f7
shivcoder9084 <shivanshrastogimzn2002@gmail.com>
2025-08-07 00:47:08 +0530

    Update hello.java

--- StudentApp/src/main/java/com/app2/entity/hello.java
+++ StudentApp/src/main/java/com/app2/entity/hello.java
@@ -3,6 +3,8 @@ package com.app2.entity;
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello bird  i am name is shivansh rastogi");
+		System.out.println("i am name is shivansh rastogi");
+		
 	}
 
 }
